e39435182a012c80a87098df718abe282d4d1688
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-08-03 19:27:07 +0300

    Ignore .vscode/ directory and update Job model

--- job/models.py
+++ job/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.db import models # type: ignore
 
 # Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=100) # Column
+    
+    
+    # image = models.ImageField(upload_to='images/')
+    # summary = models.CharField(max_length=200)
+    # title = models.CharField(max_length=200)
+    # url = models.CharField(max_length=200)
+    # def __str__(self):
+    #     return self.title
