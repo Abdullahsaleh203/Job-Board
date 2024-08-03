87f860cd566c510f310f41185d1293b66b7f73dd
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-08-03 20:34:25 +0300

    chore: Add job description and job type fields to Job model

--- job/models.py
+++ job/models.py
@@ -8,9 +8,14 @@ Model field reference¶
 https://docs.djangoproject.com/en/5.0/ref/models/fields/
 '''
 # Create your models here.
+JOB_TYPE =(
+    ('Full Time','Full Time'),
+    ('Part Time','Part Time'),
+)
 class Job(models.Model):
     title = models.CharField(max_length=100) # Column
-    
+    job_type = models.CharField(max_length=100 ,choices=JOB_TYPE)
+    description= models.TextField(max_length=1000)
     
     # image = models.ImageField(upload_to='images/')
     # summary = models.CharField(max_length=200)
