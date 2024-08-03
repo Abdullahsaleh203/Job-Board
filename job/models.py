1f4399d003e0866d496f96041c088ad5d383a58c
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-08-03 19:50:15 +0300

    chore: Register Job model in admin site

--- job/models.py
+++ job/models.py
@@ -1,5 +1,12 @@
 from django.db import models # type: ignore
 
+
+
+
+'''
+Model field reference¶
+https://docs.djangoproject.com/en/5.0/ref/models/fields/
+'''
 # Create your models here.
 class Job(models.Model):
     title = models.CharField(max_length=100) # Column
