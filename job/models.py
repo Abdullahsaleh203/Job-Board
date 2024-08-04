eae220bdc0df6f62b44f349e0cb6cddae71b39ba
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2024-08-04 12:34:24 +0300

    feat: Add Category model and register it in admin site

--- job/models.py
+++ job/models.py
@@ -18,10 +18,6 @@ JOB_TYPE =(
     ('Full Time','Full Time'),
     ('Part Time','Part Time'),
 )
-class Category(models.Model):
-        name = models.CharField(max_length=25)
-        def __str__(self):
-            return self.name
 class Job(models.Model):
     title = models.CharField(max_length=100) # Column
     job_type = models.CharField(max_length=100 ,choices=JOB_TYPE)
@@ -31,10 +27,15 @@ class Job(models.Model):
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
-
+    
     def __str__(self):
         return self.title
     
+class Category(models.Model):
+        name = models.CharField(max_length=25)
+        
+        def __str__(self):
+            return self.name
     
     # image = models.ImageField(upload_to='images/')
     # summary = models.CharField(max_length=200)
--- job/views.py
+++ job/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
+def jobs_list(request):
+    # return render(request , 'job/jobs_list.html')
+    pass
+
+
+def jobs_detali(request,id):
+    # return render(request , 'job/jobs_detali.html')
+    pass
