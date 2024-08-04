a3e1b17ddd5c264e9bc77ffb3af3565640cda7ae
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-08-04 19:40:10 +0300

    chore: Remove merge conflict markers and fix admin.py and migrations

--- job/admin.py
+++ job/admin.py
@@ -2,10 +2,6 @@ from django.contrib import admin # type: ignore
 
 # Register your models here.
 from .models import Job , Category
-<<<<<<< HEAD
-=======
-
->>>>>>> 1ecb008f78279aebdf3b19b2d00c3b073d0ccfe6
 
 admin.site.register(Job)
 admin.site.register(Category)
