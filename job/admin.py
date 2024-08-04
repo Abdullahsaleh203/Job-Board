7a83a4a39436ab973e8e596551037633322b3827
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-08-04 19:42:12 +0300

    Remove merge conflict markers and fix admin.py and migrations

--- job/admin.py
+++ job/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin # type: ignore
 
 # Register your models here.
-from .models import Job , Category
+from .models import Job
 
 admin.site.register(Job)
-admin.site.register(Category)
--- outline.md
+++ outline.md
@@ -33,4 +33,8 @@ Blog :
 contact
 home
 
+<<<<<<< HEAD
+
+=======
+>>>>>>> 1ecb008f78279aebdf3b19b2d00c3b073d0ccfe6
 login 
