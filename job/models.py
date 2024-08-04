3c21892321235947e68a2db2ab61f824badf5026
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-08-04 20:38:02 +0300

    chore: Remove commented out code and unused fields in Category model

--- job/models.py
+++ job/models.py
@@ -39,9 +39,4 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
-    # image = models.ImageField(upload_to='images/')
-    # summary = models.CharField(max_length=200)
-    # title = models.CharField(max_length=200)
-    # url = models.CharField(max_length=200)
-    # def __str__(self):
-    #     return self.title
+
