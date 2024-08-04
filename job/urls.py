5520346f276f2722db2a0a88de01855e4db3dc69
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-08-04 19:45:40 +0300

    Remove unnecessary files and code

--- job/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path , include
-
-from . import views
-
-
-urlpatterns = [
-    path('' , views.jobs_list , name='jobs_list'),
-    path('<int:id>' , views.jobs_detali , name='jobs_detali'),
-]
