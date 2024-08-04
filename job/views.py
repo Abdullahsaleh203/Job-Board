e274c837c9f39584b5f36634ef7bf2fba3b0b497
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2024-08-04 13:45:55 +0300

    Refactor Job model: Remove commented out code and add __str__ method

--- job/views.py
+++ job/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Job
 # Create your views here.
 def jobs_list(request):
+    jobs_list=Job.objects.all() # get all jobs from database
     # return render(request , 'job/jobs_list.html')
     pass
 
