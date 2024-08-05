cc27761d75a0385ef6d816f89d8b42bd58e892a6
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2024-08-05 15:22:51 +0300

    chore: Update static file directories, add static URL configuration, and media URL in settings and urls

--- project/settings.py
+++ project/settings.py
@@ -124,7 +124,10 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     "/var/www/static/",
 ]
+
+MEDIA_URL = 'media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
