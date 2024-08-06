050be62e56afa6937a8a9cf445447921da7c6eff
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2024-08-06 15:18:02 +0300

    chore: Add image field to Job model and update migrations

--- job/models.py
+++ job/models.py
@@ -14,6 +14,8 @@ https://docs.djangoproject.com/en/5.0/ref/models/fields/
     -validation
     -db size
 '''
+
+
 JOB_TYPE =(
     ('Full Time','Full Time'),
     ('Part Time','Part Time'),
@@ -28,7 +30,7 @@ class Job(models.Model):
     salary= models.IntegerField(default=0)
     experience= models.IntegerField(default=0)
     category= models.ForeignKey('Category',on_delete=models.CASCADE)
-    
+    image= models.ImageField(upload_to='jobs/')
     def __str__(self):
         return self.title
     
