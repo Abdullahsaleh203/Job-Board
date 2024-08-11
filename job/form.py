997989d888f954c146551f41b18a5f9edb0bbb7a
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2024-08-11 15:27:16 +0300

    refactor: Update job details template to use dynamic data for job title and description

---
+++ job/form.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Apply
+
+
+class ApplyForm(forms.ModelForm):
+    class Meta:
+        model = Apply
+        fields = ['name','email','website','cv','cover_letter']
+        widgets ={
+            'name':forms.TextInput(attrs={'class':'form-control'}),
+            'email':forms.EmailInput(attrs={'class':'form-control'}),
+            'website':forms.URLInput(attrs={'class':'form-control'}),
+            'cv':forms.FileInput(attrs={'class':'form-control'}),
+            'cover_letter':forms.Textarea(attrs={'class':'form-control'}),
+        }
+
+# class JobForm(forms.Form):
+#     title = forms.CharField(max_length=100)
+#     job_type = forms.ChoiceField(choices=(('Full Time','Full Time'),('Part Time','Part Time')))
+#     description= forms.CharField(widget=forms.Textarea)
+#     Vacancy= forms.IntegerField()
+#     salary= forms.IntegerField()
+#     experience= forms.IntegerField()
+#     category= forms.CharField(max_length=25)
+#     image= forms.ImageField()
