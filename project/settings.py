32e845432bd55a9756ad89a66bdc00a25631961d
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2024-08-12 10:41:42 +0300

    Refactor job details template to use dynamic data for job title and description

--- project/settings.py
+++ project/settings.py
@@ -41,9 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+ 
     # my app
     'job',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
