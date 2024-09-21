4c703218fbf9ece0f3656c7c257ed5848053eb6c
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-09-21 23:15:46 +0300

    Fix __str__ method in Profile model to return user object correctly

--- accounts/models.py
+++ accounts/models.py
@@ -13,6 +13,7 @@ class Profile(models.Model):
     # birth_date = models.DateField(null=True, blank=True)
     def __str__(self):
         return self.user
+    
 
 
 @receiver(post_save, sender=User)
