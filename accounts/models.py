d2de3b934de6c690d195231e3eb99a575d61726a
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-09-21 22:24:43 +0300

    Fix __str__ method in Profile model to return user object and remove unnecessary whitespace

--- accounts/models.py
+++ accounts/models.py
@@ -13,7 +13,8 @@ class Profile(models.Model):
     # birth_date = models.DateField(null=True, blank=True)
     def __str__(self):
         return self.user
-    
+
+
 @receiver(post_save, sender=User)
 def user_created(sender, instance, created, **kwargs):
    if created:
