a18c0e46d31efa057b98d66285ad99ec211e6d5f
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-09-21 20:33:44 +0300

    Remove commented-out fields from Profile model for clarity

--- accounts/models.py
+++ accounts/models.py
@@ -8,8 +8,6 @@ class Profile(models.Model):
     city= models.ForeignKey('City',related_name='user_city',on_delete=models.CASCADE, blank=True, null=True)
     phone_number = models.CharField(max_length=15)
     image = models.ImageField(upload_to='profile/')
-    
-    # phone_number=models.PhoneNumberField((""))
     # bio = models.TextField(max_length=500, blank=True)
     # location = models.CharField(max_length=30, blank=True)
     # birth_date = models.DateField(null=True, blank=True)
