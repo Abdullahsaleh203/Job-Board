e5ace68b4f11e14f3ba826735b17f01e4224fb8d
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-09-23 20:14:03 +0300

    Add SignupForm and update Profile model methods for user handling

--- accounts/models.py
+++ accounts/models.py
@@ -12,13 +12,13 @@ class Profile(models.Model):
     # location = models.CharField(max_length=30, blank=True)
     # birth_date = models.DateField(null=True, blank=True)
     def __str__(self):
-        return self.user
+        return str(self.user)
     
 
 @receiver(post_save, sender=User)
-def user_created(sender, instance, created, **kwargs):
+def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(User=instance)
+        Profile.objects.create(user=instance)
 class City(models.Model):
     name = models.CharField(max_length=100)
     # def __str__(self):
