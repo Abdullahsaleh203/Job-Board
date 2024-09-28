1554c2a72fa75b96870265e0fc0a37c48e70bfc9
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-09-28 18:19:55 +0300

    Add job filtering functionality and update job list template

--- job/views.py
+++ job/views.py
@@ -4,27 +4,18 @@ from django.core.paginator import Paginator
 from .form import ApplyForm , JobForm
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-
+from .filters import JobsFilter
 # Create your views here.
 
 def job_list(request) :
-    """
-    View function for displaying a list of jobs.
-
-    Parameters:
-    - request: The HTTP request object.
-
-    
-    Returns:
-    - A rendered HTML template displaying the list of jobs.
-    context: A dictionary containing the job list
-    """
     job_list=Job.objects.all()
-    paginator = Paginator(job_list, 1) # Show 25 contacts per page.
+    filter = JobsFilter(request.GET, queryset=job_list)
+    job_list = filter.qs
+    paginator = Paginator(job_list, 3) # Show 25 contacts per page.
 
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
-    context= {'jobs':page_obj} # template context name 
+    context= {'jobs':page_obj,'filter': filter} # template context name 
     return render(request,'job/job_list.html',context)
 
 def job_detail(request,slug) :
--- project/settings.py
+++ project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'job',
     'bootstrap4',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
