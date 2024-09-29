d7a0999bb735d136b429f23dce621adca199e6ae
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-09-29 23:10:14 +0300

    Add email configuration settings and documentation to notes

--- project/settings.py
+++ project/settings.py
@@ -143,3 +143,18 @@ MEDIA_ROOT =  os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER =''
+EMAIL_HOST_PASSWORD = ''
+
+# Email settings in settings.py
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'  # For Gmail
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'your-email@gmail.com'  # Your email address
+# EMAIL_HOST_PASSWORD = 'your-email-password'  # Your email password
+# DEFAULT_FROM_EMAIL = 'email@gmail.com'  # Default sender email
