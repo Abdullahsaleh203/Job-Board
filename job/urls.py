e1bbf830a85fce14c9c7ab6d6c69a82348777d47
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2024-10-01 15:28:24 +0300

    Rename job_api_list function to job_list_api and update URL to match new endpoint

--- job/urls.py
+++ job/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('',views.job_list ,name='job_list'),
     path('add',views.add_job ,name='add_job'),
     path('<str:slug>',views.job_detail ,name='job_detail'),
-    path('add',api.job_api_list ,name='job_api_list'),
+    path('api/list',api.job_list_api ,name='job_list_api'),
 ]
 
 # urlpatterns = [
