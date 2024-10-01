b0b13a0f3ce45772c374e242df7035da8d523c8c
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2024-10-01 15:24:03 +0300

    Add job_list API endpoint to retrieve all jobs

---
+++ job/api.py
@@ -0,0 +1,10 @@
+from .models import Job
+from .Serializers import JobSerializer
+from rest_framework.response import Response
+
+
+def job_list(request):
+    jobs = Job.objects.all()
+    serializer = JobSerializer(jobs, many=True)
+    return Response(serializer.data)
+
