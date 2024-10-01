a030c7c0546e728739ccff98ef386de9461daf41
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2024-10-01 11:33:08 +0300

    Enhance email content in send_message function to include sender's email

--- contact/views.py
+++ contact/views.py
@@ -11,11 +11,14 @@ def send_message(request):
         subject = request.POST['subject']
         email = request.POST['email']
         message = request.POST['message']
+        
+        message_with_email = f"From: {email}\n\nMessage:\n{message}"
         send_mail(
                 subject,
-                message,
+                message_with_email,
                 email,
-                [settings.EMAIL_HOST_USER],
+                [settings.EMAIL_HOST_USER],  
+                fail_silently=False,
             )
             
     return render(request,'contact/contact.html',{'myinfo':myinfo})
