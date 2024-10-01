05a8db6fb41d4ee55d97868311c2b33393b370bd
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2024-10-01 15:26:48 +0300

    Rename job_list function to job_api_list and update URLs to include new API endpoint

--- job/api.py
+++ job/api.py
@@ -3,7 +3,7 @@ from .Serializers import JobSerializer
 from rest_framework.response import Response
 
 
-def job_list(request):
+def job_api_list(request):
     jobs = Job.objects.all()
     serializer = JobSerializer(jobs, many=True)
     return Response(serializer.data)
--- job/urls.py
+++ job/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path ,include 
 from . import views
+from . import api
 app_name='job'
 
 
@@ -7,6 +8,7 @@ urlpatterns = [
     path('',views.job_list ,name='job_list'),
     path('add',views.add_job ,name='add_job'),
     path('<str:slug>',views.job_detail ,name='job_detail'),
+    path('add',api.job_api_list ,name='job_api_list'),
 ]
 
 # urlpatterns = [
