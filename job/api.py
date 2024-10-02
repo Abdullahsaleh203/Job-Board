50d0c73b88ad493ca4e0fc10ff249d089be26b08
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2024-10-02 12:59:47 +0300

    Add job_details_api endpoint to retrieve job details by ID

--- job/api.py
+++ job/api.py
@@ -10,4 +10,10 @@ def job_list_api(request):
     data = JobSerializer(jobs, many=True).data
     return Response({'data':data})
 
+@api_view(['GET'])
+def job_details_api(request,id):
+    job_details = Job.objects.get(id=id)
+    data = JobSerializer(job_details).data
+    return Response({'data':data})
+
 
--- job/urls.py
+++ job/urls.py
@@ -9,9 +9,6 @@ urlpatterns = [
     path('add',views.add_job ,name='add_job'),
     path('<str:slug>',views.job_detail ,name='job_detail'),
     path('api/list',api.job_list_api ,name='job_list_api'),
+    path('api/list/<int:id>',api.job_details_api ,name='job_list_api'),
 ]
 
-# urlpatterns = [
-#     path('',views.job_detail ),
-#     path('<int:id>',views.job_list ),
-# ]
