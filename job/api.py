c01ede0dc158c0317e762b912211428d72ba0d76
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2024-10-02 15:12:02 +0300

    Add class-based views for job listing and details APIs

--- job/api.py
+++ job/api.py
@@ -3,6 +3,7 @@ from .models import Job
 from .Serializers import JobSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import generics
 
 @api_view(['GET'])
 def job_list_api(request):
@@ -17,3 +18,13 @@ def job_details_api(request,id):
     return Response({'data':data})
 
 
+class JobListApi(generics.ListCreateAPIView):
+    model = Job
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+   
+
+class JobDetailsApi(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = JobSerializer
+    queryset = Job.objects.all()
+    lookup_field = 'id'
