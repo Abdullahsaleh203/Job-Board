2303f072b0b6dffacb97d337b0277f10a0d2a1c2
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-10-14 21:29:05 +0300

    Update ALLOWED_HOSTS to include Vercel domain

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-+(*b$uy&8me#*)4zcz7vnp*(*^v(!40=cnm1o+qq4t@i*n5c85
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
